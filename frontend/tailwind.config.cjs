42aa6df1f273ebf615ff1ad80d8d78d612d12a9e
Jesus-Urie1 <jesusurielvp@gmail.com>
2023-02-06 10:13:39 -0600

    first commit

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      fondo: "#b959f1",
+    },
+  },
+  plugins: [],
+};
