88b0dcc2536005443604bc3a4a228cff9cb87947
Jesus-Urie1 <jesusurielvp@gmail.com>
2023-02-08 22:40:26 -0600

    fixed colors error

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    colors: {
-      fondo: "#b959f1",
-    },
-  },
+  theme: {},
   plugins: [],
 };
