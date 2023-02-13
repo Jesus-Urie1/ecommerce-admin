dee83e5f7007f6d3087f75d33aad0e03512ec2b6
Jesus-Urie1 <jesusurielvp@gmail.com>
2023-02-12 22:47:57 -0600

    Home

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import GroupProducts from "../components/GroupProducts";
+import NavBar from "../components/NavBar";
+const Home = () => {
+  return (
+    <div className="relative">
+      <NavBar />
+      <GroupProducts />
+    </div>
+  );
+};
+
+export default Home;
