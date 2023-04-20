79eece1f9d2dc4875e951e2dc204f83e5c6d1f80
Jesus-Urie1 <jesusurielvp@gmail.com>
2023-04-19 18:03:03 -0600

    Methods PUT and GET

--- pages/api/productos.js
+++ pages/api/productos.js
@@ -4,9 +4,38 @@ import { mongooseConnect } from "@/lib/mongoose";
 export default async function handle(req, res) {
   const { method } = req;
   await mongooseConnect();
+
+  if (method === "GET") {
+    if (req.query?.id) {
+      res.json(await Producto.findOne({ _id: req.query.id }));
+    } else {
+      res.json(await Producto.find());
+    }
+  }
+
   if (method === "POST") {
-    const { titulo, descripcion, precio } = req.body;
-    const productoDoc = await Producto.create({ titulo, descripcion, precio });
+    const { titulo, descripcion, precio, imagenes } = req.body;
+    const productoDoc = await Producto.create({
+      titulo,
+      descripcion,
+      precio,
+      imagenes,
+    });
     res.json(productoDoc);
+  }
+
+  if (method === "PUT") {
+    const { _id, titulo, descripcion, precio, imagenes } = req.body;
+    await Producto.findOneAndUpdate(
+      { _id },
+      { titulo, descripcion, precio, imagenes }
+    );
+    res.json(true);
+  }
+
+  if (method === "DELETE") {
+    const { id } = req.query;
+    await Producto.deleteOne({ _id: id });
+    res.json(true);
   }
 }
