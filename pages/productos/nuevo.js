6ab08f21c245f917283c8e001f4a362e7520ac7a
Jesus-Urie1 <jesusurielvp@gmail.com>
2023-04-19 18:03:59 -0600

    ProductoForm Nuevo

--- pages/productos/nuevo.js
+++ pages/productos/nuevo.js
@@ -1,45 +1,11 @@
-import { useState } from "react";
-import axios from "axios";
 import Layout from "@/components/Layout";
+import ProductoForm from "@/components/ProductoForm";
 
 export default function NuevoProducto() {
-  const [titulo, setTitulo] = useState("");
-  const [descripcion, setDescripcion] = useState("");
-  const [precio, setPrecio] = useState(0);
-
-  async function crearProducto(e) {
-    e.preventDefault();
-    const data = { titulo, descripcion, precio };
-    await axios.post("/api/productos", data);
-  }
   return (
     <Layout>
-      <form onSubmit={crearProducto}>
-        <h1>Nuevo Producto</h1>
-        <label>Nombre del Producto</label>
-        <input
-          type="text"
-          placeholder="nombre del producto"
-          value={titulo}
-          onChange={(e) => setTitulo(e.target.value)}
-        />
-        <label>Descripción</label>
-        <textarea
-          placeholder="descripcion"
-          value={descripcion}
-          onChange={(e) => setDescripcion(e.target.value)}
-        />
-        <label>Precio</label>
-        <input
-          type="number"
-          placeholder="precio"
-          value={precio}
-          onChange={(e) => setPrecio(e.target.value)}
-        />
-        <button type="submit" className="btn-primary">
-          Guardar
-        </button>
-      </form>
+      <h1>Nuevo Producto</h1>
+      <ProductoForm />
     </Layout>
   );
 }
